07e1b296b5a08cdb5d686dfa219e7eed326e8f59
Badhon303 <badhon.alam303@gmail.com>
2023-06-06 01:15:12 +0600

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      mode: "development",
+      includeAssets: ["favicon.svg", "favicon.ico", "assets/*"],
+      manifest: {
+        name: "App name",
+        short_name: "App short name",
+        description: "App description",
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest}"],
+      },
+    }),
+  ],
 })
